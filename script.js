b28e22a39759b71a8be9988c2928a4703451492c
insti <ignisforever@gmail.com>
2018-09-30 03:04:32 +0900

    Added CSS styling

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ addButton();
 function addButton() {
 	var buttonSwitch = document.createElement("button");
 	buttonSwitch.setAttribute("id", "buttonSwitch");
+	buttonSwitch.innerHTML = "Switch Language";
 	document.body.appendChild(buttonSwitch);
 }
 
 document.getElementById("buttonSwitch").onclick = function(){
-	
+
+	var switchTo, switchText;
+
 	if (window.location.search == "?lang=jp") {
-		var switchTo = "?lang=en";
+		switchTo = "?lang=en";
 	} else if (window.location.search == "?lang=en") {
-		var switchTo = "?lang=jp";
+		switchTo = "?lang=jp";
 	} else {
-		var switchTo = "?lang=jp";
+		switchTo = "?lang=jp";
 	}
 
 	window.location = window.location.protocol + '//' + window.location.hostname + window.location.pathname + switchTo;
+
 };
